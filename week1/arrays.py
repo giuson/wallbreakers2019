cb2974a646665ddc7fce713685a30beaeed6d20d
Gianna Uson <giannauson@gmail.com>
2019-06-24 08:01:36 -0400

    Updated week1 folder

--- week1/arrays.py
+++ week1/arrays.py
@@ -1,47 +1,66 @@
 #SORT ARRAY BY PARITY
+#Given an array A of non-negative integers, 
+#return an array consisting of all the even elements of A, 
+#followed by all the odd elements of A.
 class Solution:
     def sortArrayByParity(self, A: List[int]) -> List[int]:
         evens = []
-        odds = []
+        odds = [] 
+
+        #sort even and odd elements into separate arrays
         for i in A:
             if i%2==0:
                 evens.append(i)
             else:
                 odds.append(i)
+
+        #return as one array with evens first followed by odds
         return evens+odds
 
 #TRANSPOSE MATRIX
+#The transpose of a matrix is the matrix flipped over it's main diagonal, 
+#switching the row and column indices of the matrix.
 class Solution:
     def transpose(self, A: List[List[int]]) -> List[List[int]]:
-        newArray = []
+        transposed = []
+
+        #initialize number of rows
+        #number of rows = number of elements in first array
         for i in A[0]:
-            newArray.append([])
-        for i in A:
-            x=0
-            for j in i:
-                newArray[x].append(j)
-                x+=1
-        return newArray
+            transposed.append([])
+
+        #go through each row in given matrix
+        for rows in A:
+            x=0 #index of row
+            #go through each element in each row and append to new column
+            for element in rows:
+                transposed[x].append(element)
+                x+=1 #move to next row
+
+        return transposed
 
 #FLIPPING AN IMAGE
+#Given a binary matrix A, we want to flip the image horizontally, 
+#then invert it, and return the resulting image.
 class Solution:
     def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
-        newArray=[]
-        for i in A:
-            newArray.append(i[::-1])
-            
+        newMatrix=[]
         
-        for i in newArray:
-            x=0
-            for n, j in enumerate(i):
+        #for each row in given matrix, append the inverse to new matrix
+        for i in A:
+            newMatrix.append(i[::-1])
+        
+        #go through each row and change 0's to 1's and 1's to 0's
+        for i in newMatrix:
+            #go through all elements in each row and make the switch 
+            for n, element in enumerate(i):
                 print(i)
-                if j==1:
+                if element==1:
                     i[n]=0
                 else:
                     i[n]=1
-                x+=1
 
-        return newArray
+        return newMatrix
 
                 
             
---
+++ week1/basicmath.py
@@ -0,0 +1,30 @@
+#FIZZ BUZZ
+#program that outputs the string representation of numbers from 1 to n.
+#for multiples of three it should output “Fizz” 
+#for the multiples of five output “Buzz”
+#For numbers which are multiples of both three and five output “FizzBuzz”.
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizzBuzz = []
+
+        #go through 1 to n and check if divisible by 5, 3, or both and append appropriate result
+        for i in range(1, n+1):
+            if i%15==0:
+                fizzBuzz.append("FizzBuzz")
+            elif i%5==0:
+                fizzBuzz.append("Buzz")
+            elif i%3==0:
+                fizzBuzz.append("Fizz")
+            else: fizzBuzz.append(str(i))
+
+        return fizzBuzz
+            
+#PLUS ONE
+#Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int("".join(map(str, digits))) #turn input into a number
+        newNumber = number + 1 #add one
+        output = list(map(int, str(newNumber))) #turn back into array
+        return output
+        
